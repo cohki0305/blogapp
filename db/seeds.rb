b60cd5d2ab9ea7e295b90965ca6a949911ae3960
cohki0305 <cohki0305@gmail.com>
2020-04-16 22:28:11 +0900

    facker よるダミーデータの作成

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,5 +6,9 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-Article.create({title: '新しい記事だよ', content: '素晴らしい記事です。これは大変すごいです。'})
-Article.create({title: '素晴らしい記事だよ', content: 'ああああああああああああああああああああああああああああああ'})
+10.times do
+  Article.create(
+    title: Faker::Lorem.sentence(word_count: 5),
+    content: Faker::Lorem.sentence(word_count: 100)
+  )
+end
