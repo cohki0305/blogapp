dc04939b875fa79559b63901b3de0389d75d4bf0
cohki0305 <cohki0305@gmail.com>
2020-04-17 11:09:15 +0900

    articleに作成日を表示するためのメソッドを定義

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,8 @@
 class Article < ApplicationRecord
   validates :title, presence: true
   validates :content, presence: true
+
+  def display_created_at
+    I18n.l(self.created_at, format: :default)
+  end
 end
