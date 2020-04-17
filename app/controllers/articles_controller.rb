d21ad8dd5d70e0051ef96c7c4cfe3181caf11c2e
cohki0305 <cohki0305@gmail.com>
2020-04-17 22:33:32 +0900

    flashメッセージの表示

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -14,8 +14,9 @@ class ArticlesController < ApplicationController
   def create
     @article = Article.new(article_params)
     if @article.save
-      redirect_to article_path(@article)
+      redirect_to article_path(@article), notice: '保存できたよ'
     else
+      flash.now[:error] = '保存に失敗しました'
       render :new
     end
   end
