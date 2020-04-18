960911c2c9c166ebf587e0e9c2dc3a1f4bfdb3ac
cohki0305 <cohki0305@gmail.com>
2020-04-18 21:30:55 +0900

    記事を更新できるように変更

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -25,6 +25,16 @@ class ArticlesController < ApplicationController
     @article = Article.find(params[:id])
   end
 
+  def update
+    @article = Article.find(params[:id])
+    if @article.update(article_params)
+      redirect_to article_path(@article), notice: '更新できました'
+    else
+      flash.now[:error] = '更新できませんでした'
+      render :edit
+    end
+  end
+
   private
   def article_params
     params.require(:article).permit(:title, :content)
