92503f2922c9bf9cfab9dbb213812b75f01f48cb
cohki0305 <cohki0305@gmail.com>
2020-04-23 12:55:03 +0900

    ログインしていないと使えないアクションを指定

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,6 @@
 class ArticlesController < ApplicationController
   before_action :set_article, only: [:show, :edit, :update]
+  before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
 
   def index
     @articles = Article.all
