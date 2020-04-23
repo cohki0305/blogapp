85014dc5330f8689203f237fd49c3e9bc1f60d22
cohki0305 <cohki0305@gmail.com>
2020-04-23 13:22:12 +0900

    dbのrollbackについて学ぶ

--- app/models/article.rb
+++ app/models/article.rb
@@ -3,11 +3,11 @@
 # Table name: articles
 #
 #  id         :integer          not null, primary key
-#  content    :text
-#  title      :string
+#  content    :text             not null
+#  title      :string           not null
 #  created_at :datetime         not null
 #  updated_at :datetime         not null
-#  user_id    :integer
+#  user_id    :integer          not null
 #
 # Indexes
 #
--- db/migrate/20200423035854_add_user_id_to_articles.rb
+++
@@ -1,5 +0,0 @@
-class AddUserIdToArticles < ActiveRecord::Migration[6.0]
-  def change
-    add_reference :articles, :user
-  end
-end
