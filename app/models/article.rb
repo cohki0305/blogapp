5c6cbe05b538740eff60db08031d66d45c417382
cohki0305 <cohki0305@gmail.com>
2020-04-23 13:03:01 +0900

    ユーザに紐付いた記事を作成

--- app/models/article.rb
+++ app/models/article.rb
@@ -24,6 +24,8 @@ class Article < ApplicationRecord
 
   validate :validate_title_and_content_length
 
+  belongs_to :user
+
   def display_created_at
     I18n.l(self.created_at, format: :default)
   end
--- app/models/user.rb
+++ app/models/user.rb
@@ -21,4 +21,6 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  has_many :articles, dependent: :destroy
 end
