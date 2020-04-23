6f85a082ff332b1f2273024ffb04ea5999e4b6fb
cohki0305 <cohki0305@gmail.com>
2020-04-23 13:00:34 +0900

    user_id を追加

--- app/models/article.rb
+++ app/models/article.rb
@@ -7,6 +7,11 @@
 #  title      :string
 #  created_at :datetime         not null
 #  updated_at :datetime         not null
+#  user_id    :integer
+#
+# Indexes
+#
+#  index_articles_on_user_id  (user_id)
 #
 class Article < ApplicationRecord
   validates :title, presence: true
