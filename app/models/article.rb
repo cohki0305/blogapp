51eb5142c31f375c9f83f0f9afa1a9a3e8bc45ee
cohki0305 <cohki0305@gmail.com>
2020-04-23 13:19:47 +0900

    ユーザがいない記事はない前提で書き直す

--- app/models/article.rb
+++ app/models/article.rb
@@ -30,6 +30,10 @@ class Article < ApplicationRecord
     I18n.l(self.created_at, format: :default)
   end
 
+  def author_name
+    user.display_name
+  end
+
   private
   def validate_title_and_content_length
     char_count = self.title.length + self.content.length
