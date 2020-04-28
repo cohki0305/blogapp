5acd2ea3948b6dc1bb095bc9d5b75a7bb3b44ebc
cohki0305 <cohki0305@gmail.com>
2020-04-28 16:24:21 +0900

    自分の記事に対してアクションボタンを表示するように変更

--- app/models/user.rb
+++ app/models/user.rb
@@ -23,4 +23,8 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_many :articles, dependent: :destroy
+
+  def has_written?(article)
+    articles.exists?(id: article.id)
+  end
 end
