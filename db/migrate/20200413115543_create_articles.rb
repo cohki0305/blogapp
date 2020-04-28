4ab4266a7a5537f6ab752576ff810e03a210726c
cohki0305 <cohki0305@gmail.com>
2020-04-28 17:19:51 +0900

    DBのスキーマを修正

--- db/migrate/20200413115543_create_articles.rb
+++ db/migrate/20200413115543_create_articles.rb
@@ -1,8 +1,9 @@
 class CreateArticles < ActiveRecord::Migration[6.0]
   def change
     create_table :articles do |t|
-      t.string :title
-      t.text :content
+      t.references :user, null: false
+      t.string :title, null: false
+      t.text :content, null: false
       t.timestamps
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_24_100152) do
+ActiveRecord::Schema.define(version: 2020_04_13_115543) do
 
   create_table "articles", force: :cascade do |t|
-    t.string "title"
-    t.text "content"
+    t.integer "user_id", null: false
+    t.string "title", null: false
+    t.text "content", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.integer "user_id"
     t.index ["user_id"], name: "index_articles_on_user_id"
   end
 
