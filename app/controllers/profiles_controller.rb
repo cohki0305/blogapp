be02cdc5ace1d4c357c0e9b33fe652c11df362fa
cohki0305 <cohki0305@gmail.com>
2020-05-01 21:16:08 +0900

    フォームを作成

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -6,5 +6,6 @@ class ProfilesController < ApplicationController
   end
 
   def edit
+    @profile = current_user.build_profile
   end
 end
--- app/models/profile.rb
+++ app/models/profile.rb
@@ -17,5 +17,6 @@
 #  index_profiles_on_user_id  (user_id)
 #
 class Profile < ApplicationRecord
+  enum gender: { male: 0, female: 1, other: 2 }
   belongs_to :user
 end
