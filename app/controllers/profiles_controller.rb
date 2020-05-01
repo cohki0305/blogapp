8e76840f8014a33b831def277df7e31acc4848f8
cohki0305 <cohki0305@gmail.com>
2020-05-01 21:24:13 +0900

    updateアクション

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -8,4 +8,25 @@ class ProfilesController < ApplicationController
   def edit
     @profile = current_user.build_profile
   end
+
+  def update
+    @profile = current_user.build_profile(profile_params)
+    if @profile.save
+      redirect_to profile_path, notice: 'プロフィール更新！'
+    else
+      flash.now[:error] = '更新できませんでした'
+      render :edit
+    end
+  end
+
+  private
+  def profile_params
+    params.require(:profile).permit(
+      :nickname,
+      :introduction,
+      :gender,
+      :birthday,
+      :subscribed
+    )
+  end
 end
