9cc3354e9d6a171f917bd4dee2cdb77842f8988a
cohki0305 <cohki0305@gmail.com>
2020-05-02 20:22:19 +0900

    プロフィール情報を表示

--- app/models/user.rb
+++ app/models/user.rb
@@ -30,7 +30,15 @@ class User < ApplicationRecord
   end
 
   def display_name
-    self.email.split('@').first
+    profile&.nickname || self.email.split('@').first
+  end
+
+  def birthday
+    profile&.birthday
+  end
+
+  def gender
+    profile&.gender
   end
 
   def prepare_profile
