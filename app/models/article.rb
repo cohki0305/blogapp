4df17ae3b17cd8b3a9f14fafb414477084c23741
cohki0305 <cohki0305@gmail.com>
2020-05-03 13:02:11 +0900

    アイキャッチ画像を保存できるように

--- app/models/article.rb
+++ app/models/article.rb
@@ -14,6 +14,8 @@
 #  index_articles_on_user_id  (user_id)
 #
 class Article < ApplicationRecord
+  has_one_attached :eyecatch
+
   validates :title, presence: true
   validates :title, length: { minimum: 2, maximum: 100 }
   validates :title, format: { with: /\A(?!\@)/ }
