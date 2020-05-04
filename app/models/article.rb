693764a5384c3e5176a048b2abd81ba4932255f9
cohki0305 <cohki0305@gmail.com>
2020-05-04 09:57:36 +0900

    Likeモデルを作成

--- app/models/article.rb
+++ app/models/article.rb
@@ -25,6 +25,7 @@ class Article < ApplicationRecord
   validate :validate_title_and_content_length
 
   has_many :comments, dependent: :destroy
+  has_many :likes, dependent: :destroy
   belongs_to :user
 
   def display_created_at
--- app/models/user.rb
+++ app/models/user.rb
@@ -23,6 +23,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_many :articles, dependent: :destroy
+  has_many :likes, dependent: :destroy
   has_one :profile, dependent: :destroy
 
   delegate :birthday, :age, :gender, to: :profile, allow_nil: true
