d16e0236d24e532d5cb71332885799d0ecd37c73
cohki0305 <cohki0305@gmail.com>
2020-05-04 09:23:29 +0900

    画像アップロードと表示

--- app/models/user.rb
+++ app/models/user.rb
@@ -38,4 +38,12 @@ class User < ApplicationRecord
   def prepare_profile
     profile || build_profile
   end
+
+  def avatar_image
+    if profile&.avatar&.attached?
+      profile.avatar
+    else
+      'default-avatar.png'
+    end
+  end
 end
