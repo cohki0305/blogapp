b613f2896b3656b620312fae12f0ad2fe1d93c92
cohki0305 <cohki0305@gmail.com>
2020-05-20 21:57:55 +0900

    本番環境ではdotenvを読み込まないように変更

--- config/application.rb
+++ config/application.rb
@@ -11,8 +11,10 @@ module BlogApp
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
 
-    Bundler.require(*Rails.groups)
-    Dotenv::Railtie.load
+    if Rails.env.development? || Rails.env.test?
+      Bundler.require(*Rails.groups)
+      Dotenv::Railtie.load
+    end
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
