1ba7c632da98e2fcbf0881a4c74554d9073dca36
cohki0305 <cohki0305@gmail.com>
2020-06-04 11:42:29 +0900

    content カラムを排除

--- app/models/article.rb
+++ app/models/article.rb
@@ -3,7 +3,6 @@
 # Table name: articles
 #
 #  id         :bigint           not null, primary key
-#  content    :text             not null
 #  title      :string           not null
 #  created_at :datetime         not null
 #  updated_at :datetime         not null
@@ -15,6 +14,7 @@
 #
 class Article < ApplicationRecord
   has_one_attached :eyecatch
+  has_rich_text :content
 
   validates :title, presence: true
   validates :title, length: { minimum: 2, maximum: 100 }
