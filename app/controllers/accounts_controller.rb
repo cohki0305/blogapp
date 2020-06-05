cbce6f2358dc4099b400ec6abce68290e277a314
cohki0305 <cohki0305@gmail.com>
2020-06-05 10:37:13 +0900

    プロフィールページ

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -1,5 +1,8 @@
 class AccountsController < ApplicationController
   def show
     @account = User.find(params[:id])
+    if @account = current_user
+      redirect_to profile_path
+    end
   end
 end
