c5ebd40f1f7cb50c52c28cb9df27709aa7950091
cohki0305 <cohki0305@gmail.com>
2020-06-05 13:55:33 +0900

    timelineのテンプレート

---
+++ app/controllers/timelines_controller.rb
@@ -0,0 +1,6 @@
+class TimelinesController < ApplicationController
+  before_action :authenticate_user!
+
+  def show
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: 'articles#index'
 
+  resource :timeline, only: [:show]
+
   resources :articles do
     resources :comments, only: [:new, :create]
 
