5103dbe9f9ad116e54be0115eb89c7833828fd4a
cohki0305 <cohki0305@gmail.com>
2020-06-05 17:18:33 +0900

    フォロワーの関係

--- app/models/user.rb
+++ app/models/user.rb
@@ -29,6 +29,9 @@ class User < ApplicationRecord
   has_many :following_relationships, foreign_key: 'follower_id', class_name: 'Relationship', dependent: :destroy
   has_many :followings, through: :following_relationships, source: :following
 
+  has_many :follower_relationships, foreign_key: 'following_id', class_name: 'Relationship', dependent: :destroy
+  has_many :followers, through: :follower_relationships, source: :follower
+
   has_one :profile, dependent: :destroy
 
   delegate :birthday, :age, :gender, to: :profile, allow_nil: true
