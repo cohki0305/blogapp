e051d34f3158288bd5c02b12b984893a5ac61929
cohki0305 <cohki0305@gmail.com>
2020-06-05 10:50:57 +0900

    実際にフォローできるように

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -1,7 +1,7 @@
 class AccountsController < ApplicationController
   def show
     @account = User.find(params[:id])
-    if @account = current_user
+    if @account == current_user
       redirect_to profile_path
     end
   end
--- app/models/user.rb
+++ app/models/user.rb
@@ -40,12 +40,22 @@ class User < ApplicationRecord
   end
 
   def follow!(user)
-    following_relationships.create!(following_id: user.id)
+    user_id = if user.is_a?(User)
+      user.id
+    else
+      user
+    end
+
+    following_relationships.create!(following_id: user_id)
   end
 
   def unfollow!(user)
     relation = following_relationships.find_by!(following_id: user.id)
     relation.destroy!
+  end
+
+  def has_followed?(user)
+    following_relationships.exists?(following_id: user.id)
   end
 
   def has_liked?(article)
