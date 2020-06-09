12d26be227cd210779caef343ef95475733b1d55
cohki0305 <cohki0305@gmail.com>
2020-06-09 15:12:55 +0900

    functionを定義

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -22,16 +22,20 @@ require("@rails/actiontext")
 import $ from 'jquery'
 import axios from 'axios'
 
+const handleHeartDisplay = (hasLiked) => {
+  if (hasLiked) {
+    $('.active_heart').removeClass('hidden')
+  } else {
+    $('.inactive_heart').removeClass('hidden')
+  }
+}
+
 document.addEventListener('turbolinks:load', () => {
   const dataset = $('#article-show').data()
   const articleId = dataset.articleId
   axios.get(`/articles/${articleId}/like`)
     .then((response) => {
       const hasLiked = response.data.hasLiked
-      if (hasLiked) {
-        $('.active_heart').removeClass('hidden')
-      } else {
-        $('.inactive_heart').removeClass('hidden')
-      }
+      handleHeartDisplay(hasLiked)
     })
 })
