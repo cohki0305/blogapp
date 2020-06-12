6108aa78f11fc0249f7a8bf78a964e483ed96514
cohki0305 <cohki0305@gmail.com>
2020-06-12 20:49:19 +0900

    like status の API を作成

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,6 +1,12 @@
 class LikesController < ApplicationController
   before_action :authenticate_user!
 
+  def show
+    article = Article.find(params[:article_id])
+    like_status = current_user.has_liked?(article)
+    render json: { hasLiked: like_status }
+  end
+
   def create
     article = Article.find(params[:article_id])
     article.likes.create!(user_id: current_user.id)
--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
   resources :articles do
     resources :comments, only: [:new, :create]
 
-    resource :like, only: [:create, :destroy]
+    resource :like, only: [:show, :create, :destroy]
   end
 
   resources :accounts, only: [:show] do
