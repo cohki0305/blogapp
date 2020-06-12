7159a5181529beac3754547285dd3c5f67cfc139
cohki0305 <cohki0305@gmail.com>
2020-06-12 21:11:22 +0900

    いいねの状態で表示を切り替える

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -27,6 +27,11 @@ document.addEventListener('turbolinks:load', () => {
   const articleId = dataset.articleId
   axios.get(`/articles/${articleId}/like`)
     .then((response) => {
-      console.log(response)
+      const hasLiked = response.data.hasLiked
+      if (hasLiked) {
+        $('.active-heart').removeClass('hidden')
+      } else {
+        $('.inactive-heart').removeClass('hidden')
+      }
     })
 })
