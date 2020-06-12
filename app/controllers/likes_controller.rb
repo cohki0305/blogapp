cfc137b0c41435e33bad5508f65674277bbfc5af
cohki0305 <cohki0305@gmail.com>
2020-06-12 21:39:03 +0900

    ajaxでいいねの状態

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -10,14 +10,15 @@ class LikesController < ApplicationController
   def create
     article = Article.find(params[:article_id])
     article.likes.create!(user_id: current_user.id)
-    redirect_to article_path(article)
+
+    render json: { status: 'ok' }
   end
 
   def destroy
     article = Article.find(params[:article_id])
     like = article.likes.find_by!(user_id: current_user.id)
-
     like.destroy!
-    redirect_to article_path(article)
+
+    render json: { status: 'ok' }
   end
 end
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -21,6 +21,9 @@ require("@rails/actiontext")
 
 import $ from 'jquery'
 import axios from 'axios'
+import { csrfToken } from 'rails-ujs'
+
+axios.defaults.headers.common['X-CSRF-Token'] = csrfToken()
 
 const handleHeartDisplay = (hasLiked) => {
   if (hasLiked) {
@@ -39,4 +42,26 @@ document.addEventListener('turbolinks:load', () => {
       const hasLiked = response.data.hasLiked
       handleHeartDisplay(hasLiked)
     })
+
+  $('.inactive-heart').on('click', () => {
+    axios.post(`/articles/${articleId}/like`)
+      .then((response) => {
+        console.log(response)
+      })
+      .catch((e) => {
+        window.alert('Error')
+        console.log(e)
+      })
+  })
+
+  $('.active-heart').on('click', () => {
+    axios.delete(`/articles/${articleId}/like`)
+      .then((response) => {
+        console.log(response)
+      })
+      .catch((e) => {
+        window.alert('Error')
+        console.log(e)
+      })
+  })
 })
