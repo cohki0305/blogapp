4ece9b6aea03acaac7c3ce2f041cf85f9b30e68e
cohki0305 <cohki0305@gmail.com>
2020-06-16 12:03:12 +0900

    エラー処理

--- app/javascript/packs/article.js
+++ app/javascript/packs/article.js
@@ -36,6 +36,9 @@ document.addEventListener('DOMContentLoaded', () => {
         appendNewComment(comment)
       })
     })
+    .catch((error) => {
+      window.alert('失敗！')
+    })
 
   handleCommentForm()
 
