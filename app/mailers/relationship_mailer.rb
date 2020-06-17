f4d90b7b90669fea41fc9c3bb54f280593863a64
cohki0305 <cohki0305@gmail.com>
2020-06-17 14:58:17 +0900

    Mailerの設定

--- app/mailers/relationship_mailer.rb
+++ app/mailers/relationship_mailer.rb
@@ -1,2 +1,8 @@
 class RelationshipMailer < ApplicationMailer
+  def new_follower(user, follower)
+    @user = user
+    @follower = follower
+    ActiveDecorator::Decorator.instance.decorate(User)
+    mail to: user.email, subject: "【お知らせ】フォローされました"
+  end
 end
