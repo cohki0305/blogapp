9da751d5e97a4832a8c35b23196c335129de2d63
cohki0305 <cohki0305@gmail.com>
2020-06-21 10:05:07 +0900

    コールバックでメール送付

--- app/models/relationship.rb
+++ app/models/relationship.rb
@@ -16,4 +16,11 @@
 class Relationship < ApplicationRecord
   belongs_to :follower, class_name: 'User'
   belongs_to :following, class_name: 'User'
+
+  after_create :send_email
+
+  private
+  def send_email
+    RelationshipMailer.new_follower(following, follower).deliver_now
+  end
 end
