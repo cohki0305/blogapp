ed67a865665c538461913279d4937b720c60efbb
cohki0305 <cohki0305@gmail.com>
2020-06-23 21:10:07 +0900

    sidekiqの設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
+require 'sidekiq/web'
+
 Rails.application.routes.draw do
+  mount Sidekiq::Web => '/sidekiq' if Rails.env.development?
   mount LetterOpenerWeb::Engine, at: '/letter_opener' if Rails.env.development?
 
   devise_for :users
