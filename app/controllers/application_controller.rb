95b68725fe3555d1085390774484e8268f93fda0
cohki0305 <cohki0305@gmail.com>
2020-06-26 11:03:10 +0900

    多言語化

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,17 @@
 class ApplicationController < ActionController::Base
+  before_action :set_locale
+
   def current_user
     ActiveDecorator::Decorator.instance.decorate(super) if super.present?
     super
+  end
+
+  def default_url_options
+    { locale: I18n.locale }
+  end
+
+  private
+  def set_locale
+    I18n.locale = params[:locale] || I18n.default_locale
   end
 end
