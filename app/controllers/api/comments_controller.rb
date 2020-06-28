44acddeec32a7d57340134730f1c915d7751751c
cohki0305 <cohki0305@gmail.com>
2020-06-28 10:12:50 +0900

    namespace, scopeに従いcontrollerを変更

--- app/controllers/api/comments_controller.rb
+++ app/controllers/api/comments_controller.rb
@@ -1,9 +1,4 @@
-class CommentsController < ApplicationController
-  def new
-    article = Article.find(params[:article_id])
-    @comment = article.comments.build
-  end
-
+class Api::CommentsController < Api::ApplicationController
   def index
     article = Article.find(params[:article_id])
     comments = article.comments
