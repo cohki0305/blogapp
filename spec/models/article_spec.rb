bb1ba698161133ba2c8f3233854a604ac97a0573
cohki0305 <cohki0305@gmail.com>
2020-07-09 16:51:22 +0900

    letを使う

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -2,19 +2,22 @@ require 'rails_helper'
 
 RSpec.describe Article, type: :model do
   context '2文字以上のタイトルと記事内容の場合' do
-    before do
-      user = User.create!({
+    let!(:user) do
+      User.create!({
         email: 'cohki0305@gmail.com',
         password: 'password'
       })
-      @article = user.articles.build({
+    end
+
+    let!(:article) do
+      user.articles.build({
         title: Faker::Lorem.characters(number: 10),
         content: Faker::Lorem.characters(number: 300)
       })
     end
 
     it '記事を保存できる' do
-      expect(@article).to be_valid
+      expect(article).to be_valid
     end
   end
 end
