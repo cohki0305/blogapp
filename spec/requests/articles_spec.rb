edfea95a496d13ed79803328376ca5bf2471530f
cohki0305 <cohki0305@gmail.com>
2020-07-12 10:22:45 +0900

    request spec を追加

---
+++ spec/requests/articles_spec.rb
@@ -0,0 +1,13 @@
+require 'rails_helper'
+
+RSpec.describe 'Articles', type: :request do
+  let!(:user) { create(:user) }
+  let!(:articles) { create_list(:article, 3, user: user) }
+
+  describe 'GET /articles' do
+    it '200ステータス' do
+      get articles_path
+      expect(response).to have_http_status(200)
+    end
+  end
+end
