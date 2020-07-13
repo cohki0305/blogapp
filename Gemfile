7503d0d5aefa72f175f4f94b020e3cabf6e01aef
cohki0305 <cohki0305@gmail.com>
2020-07-13 10:11:37 +0900

    factory bot の導入

--- Gemfile
+++ Gemfile
@@ -44,6 +44,7 @@ group :development, :test do
   gem 'rubocop-rails'
   gem 'dotenv-rails'
   gem 'rspec-rails'
+  gem 'factory_bot_rails'
 end
 
 group :development do
--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -1,12 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe Article, type: :model do
-  let!(:user) do
-    User.create!({
-      email: 'test@example.com',
-      password: 'password'
-    })
-  end
+  let!(:user) { create(:user) }
 
   context 'タイトルと内容が入力されている場合' do
     let!(:article) do
