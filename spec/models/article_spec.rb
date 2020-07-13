a9ed6ddcfeeef26c9a03165ef524a5e7d36ce088
cohki0305 <cohki0305@gmail.com>
2020-07-13 09:52:20 +0900

    context

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -1,16 +1,20 @@
 require 'rails_helper'
 
 RSpec.describe Article, type: :model do
-  it 'タイトルと内容が入力されていれば、記事を保存できる' do
-    user = User.create!({
-      email: 'test@example.com',
-      password: 'password'
-    })
-    article = user.articles.build({
-      title: Faker::Lorem.characters(number: 10),
-      content: Faker::Lorem.characters(number: 300)
-    })
+  context 'タイトルと内容が入力されている場合' do
+    before do
+      user = User.create!({
+        email: 'test@example.com',
+        password: 'password'
+      })
+      @article = user.articles.build({
+        title: Faker::Lorem.characters(number: 10),
+        content: Faker::Lorem.characters(number: 300)
+      })
+    end
 
-    expect(article).to be_valid
+    it '記事を保存できる' do
+      expect(@article).to be_valid
+    end
   end
 end
