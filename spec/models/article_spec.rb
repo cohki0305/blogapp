fccaf23a080fc98729c617eec23df136a7a2e05c
cohki0305 <cohki0305@gmail.com>
2020-07-14 13:28:52 +0900

    article factoryを登録

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -4,12 +4,7 @@ RSpec.describe Article, type: :model do
   let!(:user) { create(:user) }
 
   context 'タイトルと内容が入力されている場合' do
-    let!(:article) do
-      user.articles.build({
-        title: Faker::Lorem.characters(number: 10),
-        content: Faker::Lorem.characters(number: 300)
-      })
-    end
+    let!(:article) { build(:article, user: user) }
 
     it '記事を保存できる' do
       expect(article).to be_valid
@@ -17,11 +12,10 @@ RSpec.describe Article, type: :model do
   end
 
   context 'タイトルの文字が一文字の場合' do
-    let!(:article) do
-      user.articles.create({
-        title: Faker::Lorem.characters(number: 1),
-        content: Faker::Lorem.characters(number: 300)
-      })
+    let!(:article) { build(:article, title: Faker::Lorem.characters(number: 1), user: user) }
+
+    before do
+      article.save
     end
 
     it '記事を保存できない' do
