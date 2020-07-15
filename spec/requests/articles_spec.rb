7a3f5682fc6fc7d89dad7e76e2b564ea9ce48d61
cohki0305 <cohki0305@gmail.com>
2020-07-15 10:44:43 +0900

    articles#indexのテストを追加

--- spec/requests/articles_spec.rb
+++ spec/requests/articles_spec.rb
@@ -1,6 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe 'Articles', type: :request do
+  let!(:user) { create(:user) }
+  let!(:articles) { create_list(:article, 3, user: user) }
+
   describe 'GET /articles' do
     it '200ステータスが返ってくる' do
       get articles_path
