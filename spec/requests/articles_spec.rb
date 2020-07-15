cb1353c64b6f9a858e5b926f964793583c9780c7
cohki0305 <cohki0305@gmail.com>
2020-07-15 11:54:14 +0900

    ログインしていない場合のテスト

--- spec/requests/articles_spec.rb
+++ spec/requests/articles_spec.rb
@@ -25,5 +25,13 @@ RSpec.describe 'Articles', type: :request do
         expect(Article.last.content.body.to_plain_text).to eq(article_params[:content])
       end
     end
+
+    context 'ログインしていない場合' do
+      it 'ログイン画面に遷移する' do
+        article_params = attributes_for(:article)
+        post articles_path({article: article_params})
+        expect(response).to redirect_to(new_user_session_path)
+      end
+    end
   end
 end
