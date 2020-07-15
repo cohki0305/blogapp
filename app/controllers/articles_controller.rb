546392cbbcaab9ee54bc94b04e2a774ea5aaa61d
cohki0305 <cohki0305@gmail.com>
2020-07-15 10:43:42 +0900

    不要なソースコードを削除

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -13,9 +13,6 @@ class ArticlesController < ApplicationController
     @article = current_user.articles.build
   end
 
-  def like
-  end
-
   def create
     @article = current_user.articles.build(article_params)
     if @article.save
