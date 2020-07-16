1b5c8bc2b9c598b1590a8728f70ddec481129f57
cohki0305 <cohki0305@gmail.com>
2020-07-16 11:43:44 +0900

    response内容のチェック

--- spec/requests/api/comments_spec.rb
+++ spec/requests/api/comments_spec.rb
@@ -9,6 +9,12 @@ RSpec.describe 'Api::Comments', type: :request do
     it '200 Status' do
       get api_comments_path(article_id: article.id)
       expect(response).to have_http_status(200)
+
+      body = JSON.parse(response.body)
+      expect(body.length).to eq 3
+      expect(body[0]['content']).to eq comments.first.content
+      expect(body[1]['content']).to eq comments.second.content
+      expect(body[2]['content']).to eq comments.third.content
     end
   end
 end
