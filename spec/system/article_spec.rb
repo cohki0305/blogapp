21bdb6c749c1b7c1498dcd01051fcb805482b32f
cohki0305 <cohki0305@gmail.com>
2020-07-27 11:36:01 +0900

    記事一覧表示機能

--- spec/system/article_spec.rb
+++ spec/system/article_spec.rb
@@ -1,7 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe 'Article', type: :system do
+  let!(:user) { create(:user) }
+  let!(:articles) { create_list(:article, 3, user: user) }
+
   it '記事一覧が表示される' do
     visit root_path
+    articles.each do |article|
+      article = ActiveDecorator::Decorator.instance.decorate(article)
+      expect(page).to have_content article.title
+      expect(page).to have_content article.author_name
+    end
   end
 end
