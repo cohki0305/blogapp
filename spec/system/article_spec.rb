9b4eb85d748980b952b2c5708ac5a9ec4b7efa43
cohki0305 <cohki0305@gmail.com>
2020-07-27 11:37:44 +0900

    記事詳細のテスト

--- spec/system/article_spec.rb
+++ spec/system/article_spec.rb
@@ -4,12 +4,20 @@ RSpec.describe 'Article', type: :system do
   let!(:user) { create(:user) }
   let!(:articles) { create_list(:article, 3, user: user) }
 
-  it '記事一覧が表示される' do
+  it '記事一覧が表示できる' do
     visit root_path
     articles.each do |article|
       article = ActiveDecorator::Decorator.instance.decorate(article)
       expect(page).to have_content article.title
       expect(page).to have_content article.author_name
     end
+  end
+
+  it '記事詳細を表示できる' do
+    visit root_path
+    article = articles.first
+    click_on article.title
+    expect(page).to have_content article.title
+    expect(page).to have_content article.content.to_plain_text
   end
 end
